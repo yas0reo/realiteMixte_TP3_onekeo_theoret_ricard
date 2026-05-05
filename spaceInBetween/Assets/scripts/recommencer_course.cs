22531ecd9006b4e26284ae0d648ab9ad41e4f530
atplab <atplab@pm.me>
2026-05-05 17:06:00 -0400

    BUILD_TEST

---
+++ spaceInBetween/Assets/scripts/recommencer_course.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class recommencer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    //void Start()
+    //  {
+    // Button Button = this.GetComponent<Button>();
+    //  Button.onClick.AddListener(Reset);
+    //}
+
+    // public void Reset()
+    // {
+    // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    //}
+
+
+    // Update is called once per frame
+    //void Update()
+    // {
+
+    //}
+}
